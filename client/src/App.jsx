ff8c22ac3b0dc05bc14b28edbb431b01f051c6a5
Lisbeth Diaz <lizbeths.robles@gmail.com>
2024-04-02 13:11:55 -0400

    install redux

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,47 +12,47 @@ import { useReducer } from 'react'
 import ChatHistory from './components/chat-history'
 
 function App() {
-  
 
-  const [messages,setMessages] = useState([])
-  const [text,setText] = useState('')
-  const {getResponse,loading} = useChat()
-  const [open,setOpen] = useState(true)
+
+  const [messages, setMessages] = useState([])
+  const [text, setText] = useState('')
+  const { getResponse, loading } = useChat()
+  const [open, setOpen] = useState(true)
   const [conversations, setConversation] = useState()
 
 
   const sendText = async (e) => {
-      
-      setMessages((messages) => [...messages,{ text: text, isUser: true }])
-      console.log(loading)
-      let r = await getResponse(text)
-      console.log(loading)
-      console.log(r.choices[0].message.content)
-      setMessages((messages) => [...messages, { text: r.choices[0].message.content, isUser: false }]);
 
-      setText('')
+    setMessages((messages) => [...messages, { text: text, isUser: true }])
+    console.log(loading)
+    let r = await getResponse(text)
+    console.log(loading)
+    console.log(r.choices[0].message.content)
+    setMessages((messages) => [...messages, { text: r.choices[0].message.content, isUser: false }]);
 
-      // setMessages([...messages,{text:r.choices[0].message.content,isUser:false}])
+    setText('')
+
+    // setMessages([...messages,{text:r.choices[0].message.content,isUser:false}])
   }
 
-  const onEnter = (e)=> {
-    if(e.key == 'Enter'){
+  const onEnter = (e) => {
+    if (e.key == 'Enter') {
       sendText()
     }
   }
 
-  const moveBar = () =>{
-      if(open) setOpen(false)
-      else setOpen(true)
+  const moveBar = () => {
+    if (open) setOpen(false)
+    else setOpen(true)
   }
 
   const newConversation = () => {
-    setConversation((conversations) => [...conversation,messages])
+    setConversation((conversations) => [...conversation, messages])
   }
 
   return (
     <>
-    
+
       {/* <div className={`grid ${open ? 'grid-cols-p' : 'grid-cols-1'} w-screen h-screen`}> */}
       <div className={`flex flex-row w-screen h-screen `}>
         <aside className={`top-0 left-0 h-screen  bg-gray-200 ${open ? 'visible w-64 basis-64' : ' invisible w-0'}`}>
@@ -62,42 +62,42 @@ function App() {
           </div>
 
           <div className=' overflow-x-hidden max-h-[calc(100vh-5rem)]'>
-            <ChatHistory chatTitle={"holaaaaaaa"}/>
-            <ChatHistory chatTitle={"holaaaaaaa"}/>
+            <ChatHistory chatTitle={"holaaaaaaa"} />
+            <ChatHistory chatTitle={"holaaaaaaa"} />
           </div>
 
         </aside>
 
-      <div className={`flex flex-col w- h-screen basis-full  bg-slate-100`}>
-        <div className='flex  flex-row'>
-        <img src={bar} className=' w-5 mx-3' onClick={moveBar}></img> 
-        <nav className=' m-5'>
-          <p className=' text-lg'>Chat</p>
-        </nav>
-        </div>
-        <div className='flex flex-col justify-between h-full'>
-          
-            {messages.length == 0 ? <InitPage/> : <div className={`overflow-auto max-h-[calc(100vh-10rem)] mx-5`}> {messages.map((m) => <ChatBurbble text={m.text} isUser={m.isUser} loading={loading}/>)}</div>}
-          
-          <div className='flex '>
-            <input className=' m-3 w-full min-h-9 p-4 break-words border border-gray-300 rounded-lg bg-gray-400 focus:bg-gray-200 focus:ring-blue-500 focus:border-blue-500' 
-              type='text' value={text}
-              // onChange={(e) => setText(e.target.vale)}
-              onChange={(e) => setText(e.target.value)}
-              onKeyDown={(e) => onEnter(e)}
-                  
-              ></input>
-                <img className='w-8 mr-3' 
-                  src={sendIcon}
-                  onClick={sendText}
-                  >
-                  </img>
+        <div className={`flex flex-col w- h-screen basis-full  bg-slate-100`}>
+          <div className='flex  flex-row'>
+            <img src={bar} className=' w-5 mx-3' onClick={moveBar}></img>
+            <nav className=' m-5'>
+              <p className=' text-lg'>Chat</p>
+            </nav>
           </div>
+          <div className='flex flex-col justify-between h-full'>
+
+            {messages.length == 0 ? <InitPage /> : <div className={`overflow-auto max-h-[calc(100vh-10rem)] mx-5`}> {messages.map((m) => <ChatBurbble text={m.text} isUser={m.isUser} loading={loading} />)}</div>}
+
+            <div className='flex '>
+              <input className=' m-3 w-full min-h-9 p-4 break-words border border-gray-300 rounded-lg bg-gray-400 focus:bg-gray-200 focus:ring-blue-500 focus:border-blue-500'
+                type='text' value={text}
+                // onChange={(e) => setText(e.target.vale)}
+                onChange={(e) => setText(e.target.value)}
+                onKeyDown={(e) => onEnter(e)}
+
+              ></input>
+              <img className='w-8 mr-3'
+                src={sendIcon}
+                onClick={sendText}
+              >
+              </img>
+            </div>
+          </div>
+
         </div>
-       
       </div>
-      </div>
-      
+
     </>
   )
 }
